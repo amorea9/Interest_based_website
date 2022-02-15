5ec217c32403d1bf1a1f9878f252c23ec4116cf4
Alessia Amore <a.amore9393@gmail.com>
2022-02-15 18:48:05 +0100

    js test1

--- productpage.js
+++ productpage.js
@@ -1,19 +1,25 @@
-// const urlParams = new URLSearchParams(window.location.search);
-// const id = urlParams.get("id");
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
 
-// const url = "https://kea22-2fbe.restdb.io/rest/badfootfashion/" + _id;
-// //API KEY 6208141b34fd621565858416
-// const options = {
-//   headers: {
-//     "x-apikey": "6208141b34fd621565858416",
-//   },
-// };
+const url = "https://kea22-2fbe.restdb.io/rest/badfootfashion?" + id;
+//API KEY 6208141b34fd621565858416
+const options = {
+  headers: {
+    "x-apikey": "6208141b34fd621565858416",
+  },
+};
 
-// fetch(url, options)
-//   .then((res) => res.json())
-//   .then((data) => showProduct(data));
+fetch(url, options)
+  .then((res) => res.json())
+  .then((data) => showProduct(data));
 
-// function showProduct(data) {
-//   console.log(data);
-//   data.forEach(showProduct);
-// }
+function showProduct(product) {
+  console.log(product);
+  document.querySelector(".product-page-h1").textContent = product.title;
+  //   document.querySelector(
+  //     ".product-image-section img"
+  //   ).src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+  document.querySelector(".product-image-section img").alt = product.title;
+  document.querySelector(".description-line").textContent = product.description;
+  document.querySelector(".comment-box").textContent = product.userComment;
+}
