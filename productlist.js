d102d8694af2a30c1b8d544e19ae95eca1adbcb8
Maria Lundqvist <mari28rd@stud.kea.dk>
2022-02-17 14:58:53 +0100

    URL category parameters fixed (thx jonas)

--- productlist.js
+++ productlist.js
@@ -1,18 +1,14 @@
 const urlParams = new URLSearchParams(window.location.search);
 const category = urlParams.get("category");
 
-// THIS WORKS - ALL CATEGORIES
-// https://kea22-2fbe.restdb.io/rest/badfootfashion?groupby=category
+// put this link in landing page
+// productlist.html?category=TheGood
 
-// THESE WORK - SPECIFIC CATEGORIES
-// https://kea22-2fbe.restdb.io/rest/badfootfashion?groupby=category&q={%22category%22:%22TheBad%22}
+// THIS IS THE OLD WRONG LINK (BAD LINK, DONT TOUCH)
+// const url = "https://kea22-2fbe.restdb.io/rest/badfootfashion?groupby=category";
 
-// https://kea22-2fbe.restdb.io/rest/badfootfashion?groupby=category&filter=TheUgly
-
-const url = "https://kea22-2fbe.restdb.io/rest/badfootfashion?groupby=category";
 const url =
-  "https://kea22-2fbe.restdb.io/rest/badfootfashion?groupby=category&filter=" +
-  category;
+  "https://kea22-2fbe.restdb.io/rest/badfootfashion?filter=" + category;
 
 //API KEY 6208141b34fd621565858416
 const options = {
