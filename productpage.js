a003fb5098c75e2576cd1731b9a90861149dbe20
Maria Lundqvist <mari28rd@stud.kea.dk>
2022-02-17 18:33:00 +0100

    ratings FIXED (finally) +  alignment fixes

--- productpage.js
+++ productpage.js
@@ -34,7 +34,7 @@ function showProduct(product) {
   document.querySelector(".product-page-h1").textContent = product.title;
   document.querySelector(
     ".product-image-section img"
-  ).src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+  ).src = `https://carolinethostrup.dk/BFF/assets/${product.image}`;
   document.querySelector(".product-image-section img").alt = product.title;
   document.querySelector(".description-line").textContent = product.description;
   document.querySelector(".comment-box p").textContent = product.userComment;
@@ -44,20 +44,61 @@ function showProduct(product) {
     "head title"
   ).textContent = `Bad Foot Fashion | ${product.title}`;
 
-  console.log(product.comfort);
+  // ratings - comfort
+  console.log(`comfort = ${product.comfort}`);
   const comfortRating = product.comfort;
-  if (comfortRating === "4") {
-    document.querySelector(".rating-1").classList.add(".ratingActive");
+  if (comfortRating >= "1") {
+    document
+      .querySelector(".comfortRatings .rating-1")
+      .classList.add("ratingActive");
+  }
+  if (comfortRating >= "2") {
+    document
+      .querySelector(".comfortRatings .rating-2")
+      .classList.add("ratingActive");
+  }
+  if (comfortRating >= "3") {
+    document
+      .querySelector(".comfortRatings .rating-3")
+      .classList.add("ratingActive");
+  }
+  if (comfortRating >= "4") {
+    document
+      .querySelector(".comfortRatings .rating-4")
+      .classList.add("ratingActive");
+  }
+  if (comfortRating >= "5") {
+    document
+      .querySelector(".comfortRatings .rating-5")
+      .classList.add("ratingActive");
   }
 
-  // showRating();
+  // ratings - ugliness
+  console.log(`ugliness = ${product.ugliness}`);
+  const uglinessRating = product.ugliness;
+  if (uglinessRating >= "1") {
+    document
+      .querySelector(".uglinessRatings .rating-1")
+      .classList.add("ratingActive");
+  }
+  if (uglinessRating >= "2") {
+    document
+      .querySelector(".uglinessRatings .rating-2")
+      .classList.add("ratingActive");
+  }
+  if (uglinessRating >= "3") {
+    document
+      .querySelector(".uglinessRatings .rating-3")
+      .classList.add("ratingActive");
+  }
+  if (uglinessRating >= "4") {
+    document
+      .querySelector(".uglinessRatings .rating-4")
+      .classList.add("ratingActive");
+  }
+  if (uglinessRating >= "5") {
+    document
+      .querySelector(".uglinessRatings .rating-5")
+      .classList.add("ratingActive");
+  }
 }
-
-// function showRating(product) {
-//   // ratings (except it's not working and i'm tiredddd)
-//   console.log(product.comfort);
-//   const comfortRating = product.comfort;
-//   if (comfortRating === "5") {
-//     document.querySelector(".rating-1").classList.add(".ratingActive");
-//   }
-// }
