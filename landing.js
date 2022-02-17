3752cf7508cc6593f3abf701ecf7a0c2a8e53ed6
Alessia Amore <a.amore9393@gmail.com>
2022-02-17 18:03:50 +0100

    add surprise button working

--- landing.js
+++ landing.js
@@ -1,43 +1,43 @@
-// const url = "https://kea22-2fbe.restdb.io/rest/badfootfashion?groupby=category";
+const url = "https://kea22-2fbe.restdb.io/rest/badfootfashion?";
 
-// //API KEY 6208141b34fd621565858416
-// const options = {
-//   headers: {
-//     "x-apikey": "6208141b34fd621565858416",
-//   },
-// };
+//API KEY 6208141b34fd621565858416
+const options = {
+  headers: {
+    "x-apikey": "6208141b34fd621565858416",
+  },
+};
 
-// fetch(url, options)
-//   .then((res) => res.json())
-//   .then((data) => handleProductList(data));
+fetch(url, options)
+  .then((res) => res.json())
+  .then((data) => handleProductList(data));
 
-// function handleProductList(data) {
-//   console.log(data);
-//   data.forEach(showProduct);
-// }
-
-// function showProduct(product) {
-//   console.log(product);
-
-//   //grab template
-//   const template = document.querySelector("template").content;
-
-//   //clone it
-//   const myClone = template.cloneNode(true);
-
-//   //change content
-//   myClone.querySelector(".a_category").textContent = product.category;
-//   myClone
-//     .querySelector(".alink")
-//     .setAttribute("href", `productlist.html?groupby=category`);
-//   // myClone.querySelector(
-//   //   ".alink img"
-//   // ).src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
-
-//   //grab parent
-//   const parent = document.querySelector(".buttons_landing_page");
-
-//   //append it
-
-//   parent.appendChild(myClone);
-// }
+function handleProductList(data) {
+  console.log(data);
+}
+//get random ID
+function random_item(items) {
+  return items[Math.floor(Math.random() * items.length)];
+}
+//array with IDs
+const items = [
+  "620b769cf701f460000a1185",
+  "620b784ff701f460000a11a7",
+  "620b7535f701f460000a115d",
+  "620b7af5f701f460000a11e1",
+  "620b759bf701f460000a1164",
+  "620b78b9f701f460000a11b1",
+  "620b795ff701f460000a11c0",
+  "620b7612f701f460000a1175",
+  "620b7bb5f701f460000a11ec",
+  "620b72e2f701f460000a1121",
+  "620b7357f701f460000a1132",
+  "620b7463f701f460000a1149",
+  "620b7c66f701f460000a11f3",
+  "620b6a46f701f460000a100b",
+];
+//print random ID
+console.log(random_item(items));
+//insert random ID in the url
+document
+  .querySelector(".surprise_link")
+  .setAttribute("href", `productpage.html?id=${random_item(items)}`);
